3188f797cfd649ed323b8cc16d077d8b0f206693
ahmed-lotfy-dev <elshenawy19@gmail.com>
2023-03-04 23:50:50 +0200

    fix old to add it into the portfolio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-// optional
-$('#blogCarousel').carousel({
-    interval: 5000
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 4,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+var swiper = new Swiper(".work-projects", {
+  slidesPerView: 4,
+  spaceBetween: 1,
+  cssMode: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
 });
